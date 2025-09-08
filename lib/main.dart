2a6094ec4211c3671480d5e15eeb735c2255b716
Ahmed Rizk <rezk18649@gmail.com>
2025-09-08 18:19:54 +0300

    add localization & arabic language

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const MedCare());
 }
 
-class MedCare extends StatelessWidget{
+class MedCare extends StatelessWidget {
   const MedCare({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text("Welcome"),
-       ),
-     ),
-   );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+
+      locale: Locale("ar"),
+
+      home: Scaffold(),
+    );
   }
 }
