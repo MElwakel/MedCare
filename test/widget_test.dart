76f8ad3f2c7372c80b9d619688f6521592a2ca37
Ahmed Rizk <rezk18649@gmail.com>
2025-09-08 18:02:14 +0300

    add architecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:medcare/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MedCare());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
