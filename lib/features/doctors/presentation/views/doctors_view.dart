5500c6ca36e9ef602f68cf31fee859a5bb7bd167
Ahmed Rizk <rezk18649@gmail.com>
2025-09-17 17:23:33 +0300

    create doctor view

---
+++ lib/features/doctors/presentation/views/doctors_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:medcare/features/doctors/presentation/views/widgets/doctors_view_body.dart';
+
+class DoctorsView extends StatelessWidget {
+  const DoctorsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DoctorsViewBody(),
+    );
+  }
+}
+
+
---
+++ lib/features/doctors/presentation/views/widgets/doctors_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class DoctorsViewBody extends StatelessWidget {
+  const DoctorsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
